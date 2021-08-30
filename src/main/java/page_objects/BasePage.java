27c769e63fb134dac04db7ee806b1a9af39b033c
Hoan Huynh <vanhoan2006@gmail.com>
2021-08-30 13:30:43 +0700

    Completed TC01 30/8/2021

---
+++ src/main/java/page_objects/BasePage.java
@@ -0,0 +1,24 @@
+package page_objects;
+
+import helpers.DriverHelper;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+
+public class BasePage {
+
+    // Locators
+    private final By tabLogin = By.cssSelector("a[href$='Login.cshtml']");
+
+    // Elements
+    private WebElement getTabLogin() {
+        return DriverHelper.getDriver().findElement(tabLogin);
+    }
+
+    // Methods
+
+    public void clickLoginTab() {
+        this.getTabLogin().click();
+    }
+
+}
---
+++ src/main/java/page_objects/LoginPage.java
@@ -0,0 +1,47 @@
+package page_objects;
+
+import helpers.DriverHelper;
+import helpers.ElementHelper;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage extends BasePage{
+
+    // Locators
+    private final By txtUsername = By.id("username");
+    private final By txtPassword = By.id("password");
+    private final By btnLogin = By.cssSelector("input[value='Login']");
+    private final By lblWelcomeMessage = By.cssSelector("div[class='account'] > strong");
+
+
+    // Elements
+    private WebElement getTxtUsername() {
+        return DriverHelper.getDriver().findElement(txtUsername);
+    }
+
+    private WebElement getTxtPassword() {
+        return DriverHelper.getDriver().findElement(txtPassword);
+    }
+
+    private WebElement getBtnLogin() {
+        return DriverHelper.getDriver().findElement(btnLogin);
+    }
+
+    private WebElement getLblWelcomeMessage() {
+        return DriverHelper.getDriver().findElement(lblWelcomeMessage);
+    }
+
+
+    // Methods
+    public void login(String user, String password) {
+        ElementHelper.scrollTo(this.getTxtUsername());
+        this.getTxtUsername().sendKeys(user);
+        this.getTxtPassword().sendKeys(password);
+        this.getBtnLogin().click();
+    }
+
+    public String getWelcomeMessage() {
+        return this.getLblWelcomeMessage().getText();
+    }
+
+}
