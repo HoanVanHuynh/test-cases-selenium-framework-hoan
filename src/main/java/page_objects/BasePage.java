39bee72b8933c330ef7501581ccbde956b75d86d
Hoan Huynh <vanhoan2006@gmail.com>
2021-08-30 14:57:12 +0700

    I have already fixed three errors from Mr.Tan viewer

--- src/main/java/page_objects/BasePage.java
+++ src/main/java/page_objects/BasePage.java
@@ -16,7 +16,6 @@ public class BasePage {
     }
 
     // Methods
-
     public void clickLoginTab() {
         this.getTabLogin().click();
     }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -4,26 +4,22 @@ import common.Constants;
 import helpers.LogHelper;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import page_objects.BasePage;
 import page_objects.LoginPage;
 
 public class LoginTests extends BaseTest {
 
-    private BasePage basePage = new BasePage();
     private LoginPage loginPage = new LoginPage();
 
     @Test(description = "User can log into Railway with valid username and password")
     public void tc01_LoginWithValidInformation() {
         LogHelper.info("Click on Login tab");
-        basePage.clickLoginTab();
+        loginPage.clickLoginTab();
         LogHelper.info("Enter valid Email and Password and click on Login button");
         loginPage.login(Constants.USERNAME, Constants.PASSWORD);
 
         LogHelper.info("Get welcome user message");
         String actualMessage = loginPage.getWelcomeMessage();
         String expectedMessage = "Welcome " + Constants.USERNAME;
-
         Assert.assertEquals(actualMessage, expectedMessage, "Welcome user message is not displayed as expected");
-
     }
 }
