a46909a0fb29ebd15786dbcb5763438558121837
Hoan Huynh <vanhoan2006@gmail.com>
2021-08-31 16:20:31 +0700

    Completed TC04 31/10/2021

--- src/main/java/page_objects/BasePage.java
+++ src/main/java/page_objects/BasePage.java
@@ -8,14 +8,23 @@ public class BasePage {
 
     // Locators
     private final By tabLogin = By.cssSelector("a[href$='Login.cshtml']");
+    private final By tabBookTicket = By.cssSelector("a[href$='BookTicketPage.cshtml']");
 
     // Elements
     private WebElement getTabLogin() {
         return DriverHelper.getDriver().findElement(tabLogin);
     }
 
+    private WebElement getTabBookTicket() {
+        return DriverHelper.getDriver().findElement(tabBookTicket);
+    }
+
     // Methods
     public void clickLoginTab() {
         this.getTabLogin().click();
+    }
+
+    public void clickBookTicketTab() {
+        this.getTabBookTicket().click();
     }
 }
